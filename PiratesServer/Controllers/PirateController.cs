db420af11a43dd109646882bc6fd48de613dca22
Ben <bmjaspers@gmail.com>
2015-05-06 18:53:03 -0400

    Initial commit of the PiratesServer
    This is the initial commit of the Pirates server. Closely mimics the
    service availabe at http://pirate.azurewebsites.net/api/Pirate. Give a
    number of pirates and it will calculate a minimum number of coins.

---
+++ PiratesServer/Controllers/PirateController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PiratesServer.Controllers
+{
+    public class PirateController : ApiController
+    {
+        [HttpGet]
+        [Route("api/pirates/{numberOfPirates}")]
+        public IHttpActionResult Get(int numberOfPirates)
+        {
+            if (numberOfPirates < 2)
+            {
+                return this.Ok("We expect 2 or more pirates.");
+            }
+
+            // this is a special case, it can be calculated
+            // as 2^4 - 1 or n^(n + 2) - n + 1
+            // everything else is n^(n + 1) - n + 1
+            if (numberOfPirates == 2) 
+            {
+                return this.Ok(15);
+            }
+
+            var doubleOfPirates = (double)numberOfPirates;
+
+            var temp = Math.Pow(doubleOfPirates, doubleOfPirates + 1);
+
+            return this.Ok(temp - doubleOfPirates + 1);
+        }
+    }
+}
---
+++ PiratesServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PiratesServer.WebApiApplication" Language="C#" %>
